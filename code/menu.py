898a4af5023c2a6175cd8dddd5d1a26731f5a206
W1not <r.raul.rodriguez.7@gmail.com>
2025-10-30 23:42:13 -0600

    Added GUI Main menu

---
+++ code/menu.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import speechinput as si
+
+class GUI:
+    def speech_start():
+        si.speechDetector.speech_usage(1)
+
+    def visual():
+        root = tk.Tk()
+        root.title("Speech")
+        root.geometry("400x300")
+        label = tk.Label(root, text="Say exit to finish recognition")
+        label.pack()
+
+        start_speech_button = tk.Button(root, text="Start", command=GUI.speech_start)
+        start_speech_button.pack()
+        root.mainloop()
--- code/speechinput.py
+++ code/speechinput.py
@@ -1,10 +1,11 @@
 import speech_recognition as sr
-import pyttsx3
 import pyaudio
-import os
+import pyttsx3
 
 
 class speechDetector:
+    # Init tts
+    
     #Make a list of input devices
     def speech_search():
         p = pyaudio.PyAudio()
@@ -13,33 +14,30 @@ class speechDetector:
             if list_device["maxInputChannels"] >= 1:
                 print("Microfono ",  list_device["index"] , " " , list_device["name"])
 
-    def speech_configurator(index,output):
-        print(index)
-        print(output)
 
+    def text_to_speech(text):
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+    
     def speech_usage(input_device):
-        queue_speech = []
         #Inicializador de speech_recognition
         r = sr.Recognizer()
         while True:
             with sr.Microphone(device_index=input_device) as source:
                 print("Say something: ")
+                # Calling fuction to speech
                 audio = r.listen(source)
             try:
-                print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-                if "exit" in r.recognize_google(audio).lower():
+                text = r.recognize_google(audio, language="es-MX")
+                print("You said ", text)
+                speechDetector.text_to_speech(text.lower())
+                if "exit" in text.lower():
                     print("Exiting...")
                     break
-                speechDetector.text_to_speech(r.recognize_google(audio))
+
+                
             except sr.UnknownValueError:
                 print("Google Speech Recognition could not undestrand audio")
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-    def text_to_speech(text):
-        engine = pyttsx3.init()
-        engine.say(text.lower())
-        engine.runAndWait()
-
-        
-            
